7d86a6e5871028c0485bf5b6d4f21ba523557a21
suelrin <dancerelrin@gmail.com>
2023-04-17 08:05:08 +0900

    nested Routes

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { Switch, Route, useLocation, useParams } from "react-router-dom";
 import styled from "styled-components";
+import Price from "./Price";
+import Chart from "./Chart";
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -18,6 +20,28 @@ const Header = styled.header`
 const Title = styled.h1`
   font-size: 48px;
   color: ${(props) => props.theme.accentColor}
+`;
+
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  span:first-child {
+    font-size: 10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+`;
+const Description = styled.p`
+  margin: 20px 0px;
 `;
 
 const Loader = styled.div`
@@ -106,15 +130,53 @@ function Coin() {
       ).json();
       setInfo(infoData);
       setPriceInfo(priceData);
+      setLoading(false);
     })();
-  }, []);
+  }, [coinId]);
 
   return (
     <Container>
       <Header>
-        <Title>{state?.name || "Loading..."}</Title>
+        <Title>{state?.name ? state.name : loading ? "Loading..." : info?.name}</Title>
       </Header>
-      {loading ? (<Loader>Loading...</Loader>) : priceInfo?.quotes.USD.price}
+      {loading ? (<Loader>Loading...</Loader>) : 
+        (
+          <>
+            <Overview>
+              <OverviewItem>
+                <span>Rank:</span>
+                <span>{info?.rank}</span>
+              </OverviewItem>
+              <OverviewItem>
+                <span>Symbol:</span>
+                <span>${info?.symbol}</span>
+              </OverviewItem>
+              <OverviewItem>
+                <span>Open Source:</span>
+                <span>{info?.open_source ? "Yes" : "No" }</span>
+              </OverviewItem>
+            </Overview>
+            <Description>{info?.description}</Description>
+            <Overview>
+              <OverviewItem>
+                <span>Total Suply:</span>
+                <span>{priceInfo?.total_supply}</span>
+              </OverviewItem>
+              <OverviewItem>
+                <span>Max Supply:</span>
+                <span>{priceInfo?.max_supply}</span>
+              </OverviewItem>
+            </Overview>
+            <Switch>
+              <Route path={`/${coinId}/price`}>
+                <Price />
+              </Route>
+              <Route path={`/${coinId}/chart`}>
+                <Chart />
+              </Route>
+            </Switch>
+          </>
+        )}
     </Container>
   );
 }
