bee4dc28aee0ec86427d0d908a4a67056bcb698e
suelrin <dancerelrin@gmail.com>
2023-04-17 07:16:25 +0900

    data type setting

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -18,30 +18,86 @@ const Header = styled.header`
 const Title = styled.h1`
   font-size: 48px;
   color: ${(props) => props.theme.accentColor}
-`; 
+`;
 
 const Loader = styled.div`
   text-align: center;
   display: block;
 `;
 
-interface RouteParams{
+interface RouteParams {
   coinId: string;
 }
 
-interface RouteState{
+interface RouteState {
   name: string;
 }
 
-function Coin(){
+interface InfoData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  logo: string;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  first_data_at: string;
+  last_data_at: string;
+}
+
+interface PriceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: {
+      ath_date: string;
+      ath_price: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_1h: number;
+      percent_change_1y: number;
+      percent_change_6h: number;
+      percent_change_7d: number;
+      percent_change_12h: number;
+      percent_change_15m: number;
+      percent_change_24h: number;
+      percent_change_30d: number;
+      percent_change_30m: number;
+      percent_from_price_ath: number;
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
+    }
+  }
+}
+
+function Coin() {
   const [loading, setLoading] = useState(true);
-  const {coinId} = useParams<RouteParams>();
-  const {state} = useLocation<RouteState>(); 
-  const [info, setInfo] = useState({});
-  const [priceInfo, setPriceInfo] = useState({});
+  const { coinId } = useParams<RouteParams>();
+  const { state } = useLocation<RouteState>();
+  const [info, setInfo] = useState<InfoData>();
+  const [priceInfo, setPriceInfo] = useState<PriceData>();
 
   useEffect(() => {
-    (async() => {
+    (async () => {
       const infoData = await (
         await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
       ).json();
@@ -51,14 +107,14 @@ function Coin(){
       setInfo(infoData);
       setPriceInfo(priceData);
     })();
-  },[]);
+  }, []);
 
   return (
     <Container>
       <Header>
         <Title>{state?.name || "Loading..."}</Title>
       </Header>
-      {loading ? (<Loader>Loading...</Loader> ) : null}
+      {loading ? (<Loader>Loading...</Loader>) : priceInfo?.quotes.USD.price}
     </Container>
   );
 }
